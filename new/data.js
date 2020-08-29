7e99620a5ac1abfad5e4dfdf75cec99ba6b7bf2e
vadhana <vadhanacodes@gmail.com>
2020-08-29 18:28:30 +0530

    Code NewsTuck

---
+++ new/data.js
@@ -0,0 +1,46 @@
+/**
+ * This file contains the hardcoded data messages to be validated
+ */
+
+//module.exports.dataSet={
+//		
+//		a:"2",
+//		b: "3"
+//		
+//}
+module.exports={
+
+		dataDrive:{
+				
+				Curator:
+				{
+				UserName: "StageCurator",
+				Password: "$tageN3w5tuckCu6ato6"
+				
+				},
+				
+				Editor:
+				{ UserName: "stageEditor",
+				  Password: "$tageN3w5tuck3d1to6"
+				},
+				
+				unRegistered:{
+					UserName:"newCurator",
+					Password:"$stageCura$"
+				}
+},
+		errorMessages:{
+			userIDErrorMessage:"User ID is Required",
+			pwdErrorMessage: "Password is Required",
+			blankFieldsError:"Username Or Password Is Incorrect"
+			
+			
+		},
+		titles:
+			{
+			loginHeader:"newstuck"
+			
+			}
+		
+		
+}
